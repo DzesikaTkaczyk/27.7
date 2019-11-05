a4bc81067d20823d2b8e3c1155e9ebc7e7181441
Dzesika Tkaczyk <dzesika.tkaczyk@gmail.com>
2019-11-05 23:33:18 +0100

    27.7

--- redux-demo/src/Comment.js
+++ redux-demo/src/Comment.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const Comment = ({text, votes, id, thumbUpComment}) => 
+const Comment = ({text, votes, id, thumbUpComment, thumbDownComment, removeComment}) => 
 	<li>
 		{text} <span>votes: {votes}</span>
-		<button onClick={() => thumbUpComment(id)}>Thumb up</button>
+		<button onClick={() => thumbUpComment(id)}> + </button>
+		<button onClick={() => thumbDownComment(id)}> - </button>
+		<button onClick={() => removeComment(id)}> remove </button>
 	</li>;
 
 export default Comment;
